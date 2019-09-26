9d6b5243e40f42fc71a956d9a2d33065ea1237ad
LPHolanda <leandropholanda@Outlook.com>
2019-09-26 14:29:57 -0300

    ajustes

--- ComputacaoGrafica/src/atividadeA12/Bresenham.java
+++ ComputacaoGrafica/src/atividadeA12/Bresenham.java
@@ -183,67 +183,12 @@ public class Bresenham extends JFrame {
 			plot(x,y);
 		}
 	}
-	
-	public void pontosDesenho (int[] pontos) {
-		
-		int x1 = 0;
-		int y1 = 0;
-		int x2 = 0;
-		int y2 = 0;
-		
-		int countX = 0;
-		int countY = 0;
-		
-		boolean primeiraVezX = true;
-		boolean primeiraVezY = true;
-		
-		int i = 0;
-		while (i < pontos.length) {
-			
-			if (i % 2 == 0 && countX < 1) {
-				x1 = pontos[i];
-				countX++;
-				
-				if (primeiraVezX == false) {
-					x1 = x2;
-				}
-				
-				primeiraVezX = false;
-				continue;
-				
-			} else if (i % 2 == 0 && countX > 0) {
-				x2 = pontos[i];
-				countX = 0;
-			}
-			
-			if (i % 2 != 0 && countY < 1){
-				y1 = pontos[i];
-				countY++;
-				
-				if (primeiraVezY == false) {
-					y1 = y2;
-				}
-				
-				primeiraVezY = false;
-				continue;
-				
-			} else if (i % 2 != 0 && countY > 0) {
-				y2 = pontos[i];
-				countY = 0;
-				
-				desenhaReta(x1, y1, x2, y2);		
 
-			}
-			
-			i++;
-		}
-		
-	}
 	
 	public static void main(String[] args) {
 		Bresenham tarefa1 = new Bresenham();
-		tarefa1.setTitle("Tarefa 1");
 		Bresenham tarefa2 = new Bresenham();
+		tarefa1.setTitle("Tarefa 1");
 		tarefa2.setTitle("Tarefa 2");
 		
 		tarefa1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -256,73 +201,11 @@ public class Bresenham extends JFrame {
 			System.out.println(e);
 		};
 		
-		
 		tarefa1.desenhaReta(200, 200, 350, 50);
 		tarefa1.desenhaReta(200, 200, 50, 50);
 		tarefa1.desenhaReta(200, 200, 50, 350);
 		tarefa1.desenhaReta(200, 200, 350, 350);
 		
-		
 		tarefa2.desenhaQuadrante(0, 0, 700);		
-		// AGORA VAI
-//		tarefa2.desenhaReta(10, 80, 0, 650);
-//		tarefa2.desenhaReta(15, 95, 0, 650);
-//		
-//		tarefa2.desenhaReta(15, 110, 0, 650);
-//		tarefa2.desenhaReta(20, 125, 0, 650);
-//		
-//		tarefa2.desenhaReta(20, 140, 0, 650);
-//		tarefa2.desenhaReta(25, 155, 0, 650);
-//		tarefa2.desenhaReta(30, 170, 0, 650);
-//		
-//		tarefa2.desenhaReta(30, 185, 0, 650);
-//		tarefa2.desenhaReta(35, 200, 0, 650);
-//		tarefa2.desenhaReta(40, 215, 0, 650);
-//		tarefa2.desenhaReta(45, 230, 0, 650);
-//		tarefa2.desenhaReta(50, 245, 0, 650);
-//		tarefa2.desenhaReta(55, 260, 0, 650);		
-//		tarefa2.desenhaReta(60, 275, 0, 650);
-//		
-//		tarefa2.desenhaReta(70, 290, 0, 650);
-//		tarefa2.desenhaReta(75, 305, 0, 650);
-//		
-//		tarefa2.desenhaReta(80, 320, 0, 650);		
-//		tarefa2.desenhaReta(90, 335, 0, 650);
-//		tarefa2.desenhaReta(100, 350, 0, 650);
-//		tarefa2.desenhaReta(110, 365, 0, 650);
-//		tarefa2.desenhaReta(120, 380, 0, 650);
-//		tarefa2.desenhaReta(130, 395, 0, 650);
-//		tarefa2.desenhaReta(140, 410, 0, 650);
-//		tarefa2.desenhaReta(150, 425, 0, 650);
-//		tarefa2.desenhaReta(160, 440, 0, 650);
-//		tarefa2.desenhaReta(170, 455, 0, 650);
-//		
-//		tarefa2.desenhaReta(185, 470, 0, 750);
-//		tarefa2.desenhaReta(200, 485, 0, 750);
-//		tarefa2.desenhaReta(215, 500, 0, 750);
-//		tarefa2.desenhaReta(230, 515, 0, 750);
-//		
-//		tarefa2.desenhaReta(245, 530, 0, 850);
-//		tarefa2.desenhaReta(265, 545, 0, 850);
-////		tarefa2.desenhaReta(275, 553, 0, 850);
-//		tarefa2.desenhaReta(285, 560, 0, 850);
-//		tarefa2.desenhaReta(305, 575, 0, 950);
-		
-
-//		tarefa2.desenhaReta(0, 455, 212, 496);
-		
-		
-//		int y1 = 455;
-//		int x2 = 212;
-//		int y2 = 496;
-//		while(y1 < 600) {
-//			tarefa2.desenhaReta(0, y1, x, 496);
-//			System.out.println("A: 0, " + y1 + ", " + x2 + ", " + y2);
-//			
-//			y1 = y1 + 10;
-//			x2 = x2 + 10;
-//			y2 = y2 + 11;
-//			
-//		}
 	}
 }
