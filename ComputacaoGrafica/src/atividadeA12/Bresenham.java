ac4f27aca63e5c5ac037bb0e1fe2567be98bb9db
LPHolanda <leandropholanda@Outlook.com>
2019-09-29 12:13:47 -0300

    atividade bresenham finalizada

--- ComputacaoGrafica/src/atividadeA12/Bresenham.java
+++ ComputacaoGrafica/src/atividadeA12/Bresenham.java
@@ -1,36 +1,66 @@
 package atividadeA12;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
+
 
 public class Bresenham extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public Bresenham() {
-		setSize(600,600);
+		setSize(500,500);
 		setVisible(true);
 	}
 	
 	public void plot(int x1, int y1) {
 		Graphics g = getGraphics();
+		g.setColor(Color.RED);
 		g.drawOval(x1, y1, 1, 1);
 	}
 	
-	public void desenhaQuadrante(int x, int y, int raio) {
+	public void desenhaLinha(int x1, int y1, int x2, int y2) {
+		
+		int dx, dy;
+		float xInc, yInc, x, y, steps;
+		
+		dy = y1 - y2;
+		dx = x1 - x2;
+		
+		if(dy < dx) {
+			steps = Math.abs(dy);
+		} else {
+			steps = Math.abs(dx);
+		}
+		
+		x = x2;
+		y = y2;
+		
+		yInc = dy / steps;
+		xInc = dx / steps;
+		
+		while (steps != 0) {
+			steps--;
+			x = x + xInc;
+			y = y + yInc;
+			
+			plot( (int)x , (int)y );
+		}
+		
+	}
+	
+	public void desenho(int raio) {
 		int p, i, j;
-		int x1 = 10, y1 = 80, x2 = 0, y2 = 650;
 		
-		boolean primeiraVez = true;
-		
-		x = 0;
-		y = raio;
+		int x = 0;
+		int y = raio;
 		p = 1 - raio;
-		i = 5;
+		i = 20;
 		j = 0;
+		int z = 700;
 		
 		plot(x,y);
-
 		while(x < y) {
 			x = x + 1;
 			if (p < 0) {
@@ -39,148 +69,14 @@ public class Bresenham extends JFrame {
 				y = y - 1;
 				p = p + 2 * (x - y) + 1;
 			}
-			plot(-y + 707, x);
-			plot(-x + 707, y);
 			
-			if (j % 2 == 0 && x1 <= 305) {
-
-				desenhaReta(x1, y1, x2, y2);
-				
-				if (x1 == 60) {
-					y1 = y1 + 15;
-					x1 = x1 + 10;
-					desenhaReta(x1, y1, x2, y2);
-					x1 = x1 + 5;
-					
-				} else if (x1 >= 80) {
-					x1 = x1 + 10;
-					
-				} else if (x1 == 15 || x1 == 20 || x1 == 30) { 
-					//repete x1 uma vez
-					
-					y1 = y1 + 15;
-					desenhaReta(x1, y1, x2, y2);
-					x1 = x1 + 5;
-					y1 = y1 + 15;
-					
-					primeiraVez = false;
-					
-				} else if (x1 < 185){
-					x1 = x1 + 5;					
-					primeiraVez = true;
-					
-				} else {
-//					y2 = y2 + 100;
-				}
-				
-				if (primeiraVez) {
-					y1 = y1 + 15;
-					primeiraVez = true;
-				}
-				
-				if (x1 >= 170 && x1 <= 305) {
-					desenhaReta(x1, y1, x2, y2);
-					
-					int z = 1;
-					y2 = y2 + 100;
-					while (z < 5) {
-						x1 = x1 + 15;
-						y1 = y1 + 15;
-						desenhaReta(x1, y1, x2, y2);
-						
-						z++;
-					}
-					
-					z = 1;
-					x1 = x1 + 15;
-					y1 = y1 + 15;
-					y2 = y2 + 100;
-					
-					desenhaReta(x1, y1, x2, y2);
-					
-					while (z < 5) {
-						x1 = x1 + 20;
-						y1 = y1 + 15;
-						desenhaReta(x1, y1, x2, y2);
-						
-						z++;
-					}
-				}
-			}
-			
-			if (j % 11 == 0 && i < 450) {
-				desenhaReta(0, i, -y + 707, x);
-				
-				i = i + 10;
+			if (j % 10 == 0) {
+				desenhaLinha(-y+z, x, i, 600);
+				i = i + 15;
+				z = z - 5;
 			}
 
-			j = j + 1;
-		}
-		
-		x1 = 0;
-		y1 = 455;
-		x2 = 212;
-		y2 = 496;
-		while(y1 < 600) {
-			desenhaReta(0, y1, x2, y2);
-			
-			y1 = y1 + 10;
-			x2 = x2 + 13;
-			y2 = y2 + 11;
-		}
-	}
-	
-	public void desenhaReta(int x1, int y1, int x2, int y2) {
-		int dx = x2 - x1;
-		int dy = y2 - y1;
-		
-		if (dx < 0) {
-			dx = dx * -1;
-		} 
-		if (dy < 0) {
-			dy = dy * -1;
-		}
-		
-		int x = x1;
-		int y = y1;
-		
-		plot(x, y);
-		
-		int p = 2 * dy - dx;
-		
-		while(x <= x2) {
-			x = x + 1;
-			if (p < 0) {
-				p = p + 2 * dy; 
-			} else {
-				
-				if (y2 < y) {
-					y = y - 1;
-				} else {
-					y = y + 1;
-				}
-				
-				p = p + 2 * (dy - dx);
-			}
-			
-			plot(x,y);
-		}
-		
-		while(x > x2) {
-			x = x - 1;
-			if (p < 0) {
-				p = p + 2 * dy; 
-			} else {
-				
-				if (y2 < y) {
-					y = y - 1;
-				} else {
-					y = y + 1;
-				}
-				p = p + 2 * (dy - dx);
-			}
-			
-			plot(x,y);
+			j++;
 		}
 	}
 
@@ -201,11 +97,11 @@ public class Bresenham extends JFrame {
 			System.out.println(e);
 		};
 		
-		tarefa1.desenhaReta(200, 200, 350, 50);
-		tarefa1.desenhaReta(200, 200, 50, 50);
-		tarefa1.desenhaReta(200, 200, 50, 350);
-		tarefa1.desenhaReta(200, 200, 350, 350);
-		
-		tarefa2.desenhaQuadrante(0, 0, 700);		
+		tarefa1.desenhaLinha(200, 200, 350, 50);
+		tarefa1.desenhaLinha(200, 200, 50, 50);
+		tarefa1.desenhaLinha(200, 200, 50, 350);
+		tarefa1.desenhaLinha(200, 200, 350, 350);
+	
+		tarefa2.desenho(700);
 	}
 }
